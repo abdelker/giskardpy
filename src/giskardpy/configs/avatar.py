25fba5f56c862471f95bc7b1167d77f30a60d0b1
monaAK <34164926+abdelker@users.noreply.github.com>
2024-06-23 12:24:34 +0200

    update joint list

--- src/giskardpy/configs/avatar.py
+++ src/giskardpy/configs/avatar.py
@@ -14,16 +14,22 @@ class Avatar_StandAlone(Giskard):
         self.configure_CollisionMarker(in_planning_loop=True)
         self.add_fixed_joint(parent_link='map', child_link='odom_combined')
         self.register_controlled_joints([
-            #'root_to_pelvis_yaw',
+            # 'root_to_pelvis_yaw',
             'pelvis_yaw_to_pelvis_pitch',
             'pelvis_pitch_to_pelvis',
-            'pelvis_to_spine_04_yaw',
+            'pelvis_to_spine_01_yaw',
+            'spine_01_to_spine_02',
+            'spine_02_to_spine_03',
+            'spine_03_to_spine_04_yaw',
             'spine_04_yaw_to_spine_04_pitch',
             'spine_04_pitch_to_spine_04',
-            'spine_04_to_head_yaw',
+            'spine_04_to_spine_05',
+            'spine_05_to_neck_01',
+            'neck_01_to_neck_02',
+            'neck_02_to_head_yaw',
             'head_yaw_to_head_pitch',
             'head_pitch_to_head',
-            'spine_04_to_clavicle_l',
+            'spine_05_to_clavicle_l',
             'clavicle_l_to_upperarm_l_yaw',
             'upperarm_l_yaw_to_upperarm_l_pitch',
             'upperarm_l_pitch_to_upperarm_l',
@@ -33,20 +39,47 @@ class Avatar_StandAlone(Giskard):
             'lowerarm_l_to_hand_l_yaw',
             'hand_l_yaw_to_hand_l_pitch',
             'hand_l_pitch_to_hand_l',
-            #'brumbrum'
+            'spine_05_to_clavicle_r',
+            'clavicle_r_to_upperarm_r_yaw',
+            'upperarm_r_yaw_to_upperarm_r_pitch',
+            'upperarm_r_pitch_to_upperarm_r',
+            'upperarm_r_to_lowerarm_r_yaw',
+            'lowerarm_r_yaw_to_lowerarm_r_pitch',
+            'lowerarm_r_pitch_to_lowerarm_r',
+            'lowerarm_r_to_hand_r_yaw',
+            'hand_r_yaw_to_hand_r_pitch',
+            'hand_r_pitch_to_hand_r',
+            'pelvis_to_thigh_l_yaw',
+            'thigh_l_yaw_to_thigh_l_pitch',
+            'thigh_l_pitch_to_thigh_l',
+            'thigh_l_to_calf_l_yaw',
+            'calf_l_yaw_to_calf_l_pitch',
+            'calf_l_pitch_to_calf_l',
+            'calf_l_to_foot_l_yaw',
+            'foot_l_yaw_to_foot_l_pitch',
+            'foot_l_pitch_to_foot_l',
+            'pelvis_to_thigh_r_yaw',
+            'thigh_r_yaw_to_thigh_r_pitch',
+            'thigh_r_pitch_to_thigh_r',
+            'thigh_r_to_calf_r_yaw',
+            'calf_r_yaw_to_calf_r_pitch',
+            'calf_r_pitch_to_calf_r',
+            'calf_r_to_foot_r_yaw',
+            'foot_r_yaw_to_foot_r_pitch',
+            'foot_r_pitch_to_foot_r',
+            # 'brumbrum'
         ])
         self.add_omni_drive_joint(parent_link_name='odom_combined',
-                             child_link_name='root',
-                             name='brumbrum',
-                             translation_limits={
-                                 Derivatives.velocity: 0.4,
-                                 Derivatives.acceleration: 1,
-                                 Derivatives.jerk: 5,
-                             },
-                             rotation_limits={
-                                 Derivatives.velocity: 0.2,
-                                 Derivatives.acceleration: 1,
-                                 Derivatives.jerk: 5
-                             }
-                             )
-    
+                                  child_link_name='root',
+                                  name='brumbrum',
+                                  translation_limits={
+                                      Derivatives.velocity: 0.4,
+                                      Derivatives.acceleration: 1,
+                                      Derivatives.jerk: 5,
+                                  },
+                                  rotation_limits={
+                                      Derivatives.velocity: 0.2,
+                                      Derivatives.acceleration: 1,
+                                      Derivatives.jerk: 5
+                                  }
+                                  )
